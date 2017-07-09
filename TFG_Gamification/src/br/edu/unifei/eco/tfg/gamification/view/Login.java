402bfe05bafe98377aa2c25fdff81742e3dd42e9
User <User@HANNspree>
2017-07-08 23:40:26 -0300

    App class redirects user to the Login page.

--- TFG_Gamification/src/br/edu/unifei/eco/tfg/gamification/view/Login.java
+++ TFG_Gamification/src/br/edu/unifei/eco/tfg/gamification/view/Login.java
@@ -10,26 +10,45 @@ import com.codename1.ui.Container;
 import com.codename1.ui.Form;
 import com.codename1.ui.Label;
 import com.codename1.ui.TextField;
-import com.codename1.ui.layouts.BorderLayout;
+import com.codename1.ui.events.ActionEvent;
+import com.codename1.ui.events.ActionListener;
 import com.codename1.ui.layouts.FlowLayout;
+import com.codename1.ui.layouts.GridLayout;
 
 /**
  *
  * @author User
  */
 public class Login extends Form{
-    private Container cnt = new Container(new FlowLayout());
+    private Container cnt = new Container(new GridLayout(4,1));
     private final Label lblUser = new Label("User");
     private final Label lblPassword = new Label("Password");
     private final TextField txfUser = new TextField("","Your e-mail",30,TextField.EMAILADDR);
     private final TextField txfPassword = new TextField("","Your password",30,TextField.PASSWORD);
+    private final Button btnClear = new Button("Clear");
     private final Button btnStart = new Button("Let's Start!");
+    private boolean login = false;
     public Login(){
         super("Welcome", new FlowLayout());
-        this.add(lblUser);
-        this.add(txfUser);
-        this.add(lblPassword);
-        this.add(txfPassword);
+        cnt.add(lblUser);
+        cnt.add(txfUser);
+        cnt.add(lblPassword);
+        cnt.add(txfPassword);
+        this.add(cnt);
+        btnClear.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent evt) {
+                txfUser.clear();
+                txfPassword.clear();
+            }
+        });
+        this.add(btnClear);
+        btnStart.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent evt) {
+                //Validate the login information
+            }
+        });
         this.add(btnStart);
         this.show();
     }
