fbe39ea2dead8a7958cb016738878b3f93f39eda
wkuan <wkuan@fitec.org.br>
2017-10-01 22:55:32 -0300

    Single Party form added (but it can be modified easily to handle lists).

--- TFG_Gamification/src/br/edu/unifei/eco/tfg/gamification/control/HomePartyButtonListener.java
+++ TFG_Gamification/src/br/edu/unifei/eco/tfg/gamification/control/HomePartyButtonListener.java
@@ -5,23 +5,36 @@
  */
 package br.edu.unifei.eco.tfg.gamification.control;
 
-import com.codename1.io.Log;
+import br.edu.unifei.eco.tfg.gamification.view.Party;
+import br.edu.unifei.eco.tfg.gamification.view.TemplateForm;
 import com.codename1.ui.Form;
 import com.codename1.ui.events.ActionEvent;
 import com.codename1.ui.events.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
  * @author User
  */
 public class HomePartyButtonListener implements ActionListener{
-    private Form hi;
+    private Form parentForm = null;
+    private Form descendantForm = null;
     public HomePartyButtonListener(Form hi){
-        this.hi = hi;
+        parentForm = hi;
     }
     @Override
     public void actionPerformed(ActionEvent e) {
-        String text = e.getSource().toString();
-        Log.p("Event source: "+text+" > Handled by HomePartyButtonListener");
+//        String text = e.getSource().toString();
+//        Log.p("Event source: "+text+" > Handled by HomePartyButtonListener");
+        if(descendantForm != null){
+            descendantForm.showBack();
+        }else{
+            //Test Begin (Remove it when real data exists)
+            List<String> list = new ArrayList<>(); 
+            for(int i=0;i<5;i++)list.add("Member "+(i+1));
+            descendantForm = new Party((TemplateForm) parentForm,"A Party Name","The Owner",list,"A brief description about the party.");
+            //Test End
+        }
     }
 }
