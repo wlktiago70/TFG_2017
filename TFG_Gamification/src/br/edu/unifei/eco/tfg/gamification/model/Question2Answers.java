10d1e9ab127f8d8f43bbe4f4879887cbae96886d
Paulo <Paulo@192.168.15.13>
2017-11-08 11:48:33 -0200

    Questions

--- TFG_Gamification/src/br/edu/unifei/eco/tfg/gamification/model/Question2Answers.java
+++ TFG_Gamification/src/br/edu/unifei/eco/tfg/gamification/model/Question2Answers.java
@@ -12,13 +12,18 @@ import java.util.List;
  *
  * @author Paulo
  */
-public class Question2Answers extends Question {
+public class Question2Answers implements Question {
+    private final String text;
+    private int scores[];
+    private boolean answered;
     private List<String> answers;
     private List<int[]> points;
     
     
     public Question2Answers(String text, String answerA, int[] pointsA, String answerB, int[] pointsB) {
-        super(text);
+        this.text = text;
+        this.answered = false;
+        
         this.answers = new ArrayList<>();
         this.points = new ArrayList<>();
         
@@ -29,11 +34,33 @@ public class Question2Answers extends Question {
         
     }
 
+    
+    @Override
+    public String getText() {
+        return this.text;
+    }
+
+    @Override
+    public int[] getScores() {
+        return this.scores;
+    }
+
+    @Override
+    public boolean isAnswered() {
+        return this.answered;
+    }
+
+    @Override
+    public List<String> getAnswers() {
+        return this.answers;
+    }
+    
     @Override
     public void answer(String answer) {
-        setScores(points.get(this.answers.indexOf(answer)));
-        setAnswered(true);
+        this.scores = points.get(this.answers.indexOf(answer));
+        this.answered = true;
     }
+
     
     
     
--- TFG_Gamification/src/br/edu/unifei/eco/tfg/gamification/model/Question4Answers.java
+++ TFG_Gamification/src/br/edu/unifei/eco/tfg/gamification/model/Question4Answers.java
@@ -12,12 +12,18 @@ import java.util.List;
  *
  * @author Paulo
  */
-public class Question4Answers extends Question {
+public class Question4Answers implements Question {
+    private final String text;
+    private int scores[];
+    private boolean answered;
     private List<String> answers;
     private List<int[]> points;
     
+    
     public Question4Answers(String text, String answerA, int[] pointsA, String answerB, int[] pointsB, String answerC, int[] pointsC, String answerD, int[] pointsD) {
-        super(text);
+        this.text = text;
+        this.answered = false;
+        
         this.answers = new ArrayList<>();
         this.points = new ArrayList<>();
         
@@ -31,12 +37,33 @@ public class Question4Answers extends Question {
         this.points.add(pointsD);
     }
 
+
     @Override
-    public void answer(String answer) {
-        setScores(points.get(this.answers.indexOf(answer)));
-        setAnswered(true);
+    public String getText() {
+        return this.text;
+    }
+
+    @Override
+    public int[] getScores() {
+        return this.scores;
+    }
+
+    @Override
+    public boolean isAnswered() {
+        return this.answered;
+    }
+
+    @Override
+    public List<String> getAnswers() {
+        return this.answers;
     }
     
+    @Override
+    public void answer(String answer) {
+        this.scores = points.get(this.answers.indexOf(answer));
+        this.answered = true;
+    }
+
     
     
     
